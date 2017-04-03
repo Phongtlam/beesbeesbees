2f97a1680db55b31918cf4f9ee48d67544d916ad
Hack Reactor Student <student@hackreactor.com>
2017-04-03 15:21:44 -0700

    Done with beez

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,25 @@
 var HoneyMakerBee = function() {
+	Bee.call(this);
+	this.age = 10;
+	this.job = "make honey";
+	this.honeyPot = 0;
+};
+
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+// here we could use Object.create(Grub.prototype) but if Bee had 
+//some of its own methods, this way would not inherit them.
+
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+	this.honeyPot++;
+};
+
+HoneyMakerBee.prototype.giveHoney = function() {
+	if (this.honeyPot > 0) {
+		this.honeyPot--;
+	} else {
+		return;
+	}
 };
 
